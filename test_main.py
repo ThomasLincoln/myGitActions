b3fba164fe30e3b8ecad78241ae2f20a5c10c180
Thomas Lincoln <66880989+ThomasLincoln@users.noreply.github.com>
2024-02-24 11:16:07 -0300

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,15 @@
+# test_greetings.py
+
+import pytest
+
+def test_hello_prints_hi():
+  """Teste se a função hello imprime "hi"."""
+  with pytest.capture_output() as captured:
+    hello()
+  assert captured.out == "hi\n"
+
+def test_bye_prints_bye():
+  """Teste se a função bye imprime "bye"."""
+  with pytest.capture_output() as captured:
+    bye()
+  assert captured.out == "bye\n"
